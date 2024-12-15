fb3925dfa483451631996218690a5bb65a93189f
NurkhanKenesov <nurkhan200505@gmail.com>
2024-12-15 19:27:34 +0500

    Comparator

---
+++ UniversitySystem/src/Comparator/SortbyCitations.java
@@ -0,0 +1,8 @@
+package Comparator;
+
+class SortbyCitations implements Comparator<Citations> {
+    @Override
+    public int compare(Citations p1, Citations p2) {
+        return Integer.compare(p1.getCitations(), p2.getCitations());
+    }
+}
---
+++ UniversitySystem/src/Comparator/SortbyPages.java
@@ -0,0 +1,8 @@
+package Comparator;
+
+public class SortbyPages implements Comparator <Book> {
+	    public int compare(Book b1, Book b2) {
+	        return Integer.compare(b1.getPages(), b2.getPages());
+	    }
+	}
+
